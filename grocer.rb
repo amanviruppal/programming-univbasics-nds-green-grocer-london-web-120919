b710dcd37b0e5c554fd4f808b9cf12216f37a4e5
amanvir uppal <amanvir.uppal@icloud.com>
2019-11-14 09:33:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -70,7 +70,9 @@ def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart)
   final_cart = apply_clearance(couponed_cart)
+  
   total = 0 
+  
   
 end
 

240d53f09518061aa1411d3d9ce1711b9eb02493
amanvir uppal <amanvir.uppal@icloud.com>
2019-11-14 09:45:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -77,7 +77,7 @@ def checkout(cart, coupons)
   total += final_cart[counter][:price] * final_cart[counter][:count]
   counter += 1 
  end 
- if > 100 
+ if total > 100 
    total -= (total * 0.10)
  end 
  total 
